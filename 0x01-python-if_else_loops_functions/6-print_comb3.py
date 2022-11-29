db77784365da3142b3404952e394578fcc2d6434
jainam12345 <jainamsharma7@gmail.com>
2022-11-29 12:15:54 -0800

    new

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,8 +2,6 @@
 for i in range(10):
     for j in range(10):
         if (i < j):
-            print("{}{}".format(i,j), end='')
+            print("{}{}".format(i, j), end='')
             if (i + 1 != 9):
                 print(", ", end='')
-            else:
-                print('\n')

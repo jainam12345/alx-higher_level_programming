a996d39902dadf6ba43817c74740995e840de46a
jainam12345 <jainamsharma7@gmail.com>
2022-12-07 04:32:59 -0800

    task 0

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new_matrix = []
+    for i in matrix:
+        new_row = list(map(lambda x : x ** 2, i))
+        new_matrix.append(new_row)
+    return new_matrix

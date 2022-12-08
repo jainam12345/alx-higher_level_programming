95e1b90d95a5734a048feca8f5842af15c5d8eaf
jainam12345 <jainamsharma7@gmail.com>
2022-12-08 04:17:02 -0800

    new

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
     if a_dictionary:
-        return max(a_dictionary.items())[0]
+        max_value = max(a_dictionary.values())
+        max_key = ''
+        for i in a_dictionary.keys():
+            if a_dictionary[i] == max_value:
+                max_key = i
+                return max_key
     else:
         return None
